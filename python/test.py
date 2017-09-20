b70c09144d537cfa93250198cdc580c1ce8e92d5
Felipe Aleixo <felipepaleixo@hotmail.com>
2017-09-20 16:48:05 -0300

    Update test.py functions messages and assert
    new function it added

--- python/test.py
+++ python/test.py
@@ -1,10 +1,14 @@
-# Test module for the challenges
+# Test module
 
-# Describe the test you're gonna do
-def describe(message):
-    print("----- " + message + " -----")
+# Describe the test examples
+def describe(message):    
+    print(message)
+
+# Describe the current test case
+def it(message):
+    print("- " + message + ": ")
 
 # Assert the values
-def assert_equals(inp, expected, msg=""):
-    if msg: print(msg)
-    print("Correct" if inp == expected else "Incorrect")
+def assert_equals(actual, expected, message=""):
+    if message = print(message)
+    print(("%s should equal %s" %(str(actual), str(expected))) if actual != expected else "Test passed")
