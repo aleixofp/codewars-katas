aace81a61c4094e49bfc790fa2c165e11e200165
Felipe Aleixo <felipepaleixo@hotmail.com>
2017-09-20 17:09:33 -0300

    Update main.py

--- python/find-the-lucky-numbers/main.py
+++ python/find-the-lucky-numbers/main.py
@@ -1,6 +1,3 @@
-# Problem: https://www.codewars.com/kata/580435ab150cca22650001fb
-# Title: Find the lucky numbers
-# Level: 7 kyu
 # Python 3.4
 # Check DESCRIPTION.md for the problem's full description.
 
